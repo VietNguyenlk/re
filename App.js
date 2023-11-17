681204f80cb16e8e9b3213d678b8e5728f1dcda7
viet <viet1282002.com>
2023-11-18 00:19:47 +0700

    update

--- App.js
+++ App.js
@@ -1,48 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { createStore } from 'redux';
-
-
-// actions object
-var increment = {type:'increment'} // or function return object
-var initi = {count:0}
-function reducer(state=initi,action){
-  switch(action.type){
-    case 'increment' : return {
-      count : state.count +1
-    }
-    default:
-        return state;
-  }
-};
-var store = createStore(reducer);
+import increment from './redux/actions';
+// import reducer from './redux/reducer';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import Counter from './Counter';
 
 
 
 
-
-export default function App() {
+export default function App()
+ {
   var [count,setCount] =useState(0);
+ 
   return (
-    <View style={styles.container}>
-      <Text>this count is: {count}</Text>
-      <Pressable style={{width:100,height:100,backgroundColor: 'gray'}} onPress={()=>{
-        store.dispatch(increment);
-        console.log(store.getState());
-        setCount(store.getState().count)
-      }}>
-      </Pressable>
-    </View>
+    <Provider store={store}> 
+
+      <Counter/>
+      <Counter/>
+    </Provider>
   );
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
